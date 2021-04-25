4b695bbda2acfb39b85a0c209fa1c915435cc8ef
pauldub04 <paul.dubnyakov@gmail.com>
2021-04-25 12:10:02 +0300

    added vuetify

--- vue_frontend/src/App.vue
+++ vue_frontend/src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+  <!-- <v-navigation-drawer app>
+  </v-navigation-drawer>
+  <v-app-bar app>
+  </v-app-bar> -->
+
+  <v-main>
+    <v-container fluid>
+      <router-link to="/">main</router-link>
+      <router-link to="/about">about</router-link>
+
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <!-- <v-footer app>
+  </v-footer> -->
+</v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
