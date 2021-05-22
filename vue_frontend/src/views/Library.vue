00b882af132484a2751bd28725c63ce20b54869d
pauldub04 <paul.dubnyakov@gmail.com>
2021-05-22 22:40:38 +0300

    toggle add book btn

--- vue_frontend/src/views/Library.vue
+++ vue_frontend/src/views/Library.vue
@@ -7,8 +7,11 @@
           <h1> {{ lib.name }} </h1>
           <p> {{ lib.description }} </p>
         </v-col>
+        <v-btn v-if="lib.creator_id == this.$store.getters.getUser.id" class="mt-10" @click='addBook = !addBook'>
+          Добавить книгу
+        </v-btn>
       </v-row>
-      
+
       <v-row v-if="books.length == 0">
         <v-col>
           <p> В этой библиотеке ещё нет книг </p>
@@ -16,39 +19,50 @@
       </v-row>
 
       <div v-else>
-        <v-row>
-          <v-col cols="auto" class="ml-auto">
-            <v-btn-toggle
-              v-model="toggle_view"
-              mandatory
-              group
-            >
-              <v-btn>
-                <v-icon>mdi-format-list-bulleted-square</v-icon>
-              </v-btn>
-              <v-btn>
-                <v-icon>mdi-view-grid</v-icon>
-              </v-btn>
-            </v-btn-toggle>
-          </v-col>
-        </v-row>
+        <div v-if="!addBook">
+          <v-row>
+            <v-col cols="auto" class="ml-auto">
+              <v-btn-toggle
+                v-model="toggle_view"
+                mandatory
+                group
+              >
+                <v-btn>
+                  <v-icon>mdi-format-list-bulleted-square</v-icon>
+                </v-btn>
+                <v-btn>
+                  <v-icon>mdi-view-grid</v-icon>
+                </v-btn>
+              </v-btn-toggle>
+            </v-col>
+          </v-row>
 
-        <v-divider class="mb-10"></v-divider> 
+          <v-divider class="mb-10"></v-divider> 
 
-        <v-row 
-          class="mb-15"
-        >
-          <v-col>
-            <Table
-              v-if="toggle_view % 2 == 0"
-              :books="books"
-            />
-            <List
-              v-else
-              :books="books"
-            />
-          </v-col>
-        </v-row>
+          <v-row 
+            class="mb-15"
+          >
+            <v-col>
+              <Table
+                v-if="toggle_view % 2 == 0"
+                :books="books"
+              />
+              <List
+                v-else
+                :books="books"
+              />
+            </v-col>
+          </v-row>
+        </div>
+        <div v-else>
+          Добавление книги
+          <v-btn color="success" @click="save">
+            Cохранить
+          </v-btn>
+          <v-btn @click="addBook = false">
+            Отмена
+          </v-btn>
+        </div>
       </div>
 
     </v-main>
@@ -70,6 +84,7 @@ export default {
     books: [],
     lib: null,
     toggle_view: 0,
+    addBook: false,
   }),
   mounted() {
     this.toggle_view = JSON.parse(localStorage.getItem("library_view_type")) || 0;
@@ -86,11 +101,15 @@ export default {
       this.books = response.data
     });
   },
+  methods: {
+    save() {
+      this.addBook = false
+    }
+  },
   watch: {
     toggle_view() {
-      console.log(this.toggle_view)
       localStorage.setItem("library_view_type", this.toggle_view);
     },
-  }
+  },
 }
 </script>
